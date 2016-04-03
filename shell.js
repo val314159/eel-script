a1dc43d88200e083312f6fbe365e5a4b03521d19
Joel Ward <jmward@gmail.com>
2016-04-03 15:40:13 -0700

    tighten up code

--- shell.js
+++ shell.js
@@ -13,30 +13,20 @@ function runShell(c){
 
 function execstr(pfx,c){
     print(pfx+" c ", c)
-    try{
-	var r = eval(c)
-	print(pfx+" r ", r)
-	return r
-    }catch(e){
-	print(pfx+" e ", e)
-	return e}}
+    try{ var r=eval(c); print(pfx+" r ", r); return r }
+    catch(e){ print(pfx+" e ", e); return e}}
 
 function evalstr(pfx,i){
     print(pfx+" i ", str(i))
-    if     (i.startsWith('!')){return runShell(i.slice(1))}
-    else if(i.startsWith('~')){return execstr(pfx,i.slice(1))}
-    else if(i.startsWith('(')){
-	var p = Parse(i)
-	print(pfx+" p ", p)
-	return execstr(pfx,Compile.File(p))
-    }else{
-	var p = Parse(i)
-	print(pfx+" p ", p)
-	return execstr(pfx,Compile.File(p))}}
+    if     ( i.startsWith('!')) return runShell(i.slice(1))
+    else if( i.startsWith('~')) return execstr(pfx,i.slice(1))
+    else if(!i.startsWith('(')) i="("+i+")"
+    var p = Parse(i)
+    print(pfx+" p ", p)
+    return execstr(pfx,Compile.File(p))}
 
 function load(filename){
     print("F f ", filename);
-    return evalstr("F",require('fs')
-		   .readFileSync(filename).toString())}
+    return evalstr("F",require('fs').readFileSync(filename).toString())}
 
 require('./repl.js')(function(i){return evalstr('.',i)})()
--- test.el
+++ test.el
@@ -1,6 +1,5 @@
-console.log
-load
-12
-34
+ console.log
+ load
+ 12
+ 34
 !ls
-!jjkhjkh
